59636869b9add4185b18163c6bb2064ebe63972a
PeizuLi5 <peizu.li@sjsu.edu>
2024-11-24 17:30:44 -0800

    step tracker home ui

--- app/src/main/java/edu/cmpe277/smarthealth/ui/home/HomeFragment.java
+++ app/src/main/java/edu/cmpe277/smarthealth/ui/home/HomeFragment.java
@@ -5,17 +5,19 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
-import android.content.res.Configuration;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import com.google.android.material.progressindicator.CircularProgressIndicator;
 
 import edu.cmpe277.smarthealth.databinding.FragmentHomeBinding;
 
@@ -24,7 +26,11 @@ public class HomeFragment extends Fragment {
     private FragmentHomeBinding binding;
     private BroadcastReceiver broadcastReceiver;
 
+    private int totalStepsCurrent;
+    private int targetSteps = 8000;
+
     private TextView stepCountTextView;
+    private CircularProgressIndicator stepProgressBar;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -34,14 +40,19 @@ public class HomeFragment extends Fragment {
         View root = binding.getRoot();
 
         stepCountTextView = binding.stepCountTextView;
+        stepProgressBar = binding.stepProgressBar;
+
+        stepProgressBar.setMax(targetSteps);
 
         loadCurrentStepCount();
 
         broadcastReceiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
-                int totalStepsCurrent = intent.getIntExtra("totalStepsCurrent", 0);
-                stepCountTextView.setText("Steps: " + totalStepsCurrent);
+                totalStepsCurrent = intent.getIntExtra("totalStepsCurrent", 0);
+                stepCountTextView.setText("" + totalStepsCurrent);
+
+                updateProgreeBar();
             }
         };
 
@@ -53,8 +64,20 @@ public class HomeFragment extends Fragment {
 
     private void loadCurrentStepCount() {
         SharedPreferences sharedPreferences = requireContext().getSharedPreferences("StepCountPref", Context.MODE_PRIVATE);
-        int totalStepsToday = sharedPreferences.getInt("totalStep", 0);
-        stepCountTextView.setText("Steps: " + totalStepsToday);
+        totalStepsCurrent = sharedPreferences.getInt("totalStep", 0);
+        stepCountTextView.setText("" + totalStepsCurrent);
+
+        updateProgreeBar();
+    }
+
+    private void updateProgreeBar() {
+        if(totalStepsCurrent >= targetSteps){
+            stepProgressBar.setProgress(targetSteps);
+            stepProgressBar.setIndicatorColor(Color.rgb(42, 219, 48));
+        }
+        else{
+            stepProgressBar.setProgress(totalStepsCurrent);
+        }
     }
 
     @Override
